c39e5cfa84c49ecded89b062dae07e8dd8496bde
Harvey <harveywyatt@hotmail.com>
2020-12-04 21:59:08 +1030

    Cleanup

--- src/main/java/me/raddari/minesweeper/client/Minesweeper.java
+++ src/main/java/me/raddari/minesweeper/client/Minesweeper.java
@@ -4,6 +4,8 @@ import me.raddari.minesweeper.controller.GameController;
 import me.raddari.minesweeper.controller.StandardGame;
 import me.raddari.minesweeper.view.MinesweeperView;
 
+import javax.swing.SwingUtilities;
+
 public final class Minesweeper implements Runnable {
 
     private MinesweeperView view;
@@ -17,6 +19,10 @@ public final class Minesweeper implements Runnable {
         controller = new StandardGame(16, 16, 50);
         view = new MinesweeperView(controller, 1920, 1080);
         controller.generateBombs(1, 1);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Minesweeper());
     }
 
 }
--- src/main/java/me/raddari/minesweeper/view/MinesweeperView.java
+++ src/main/java/me/raddari/minesweeper/view/MinesweeperView.java
@@ -5,91 +5,30 @@ import org.jetbrains.annotations.NotNull;
 
 import javax.swing.JFrame;
 import javax.swing.WindowConstants;
-import javax.swing.event.MouseInputListener;
 import java.awt.Dimension;
 import java.awt.GridLayout;
-import java.awt.event.*;
 
-public final class MinesweeperView implements MouseInputListener, MouseWheelListener, KeyListener {
+public final class MinesweeperView {
 
-    private final GameController gameController;
+    private final GameController controller;
     private final JFrame frame;
     private MinefieldPanel minefieldPanel;
 
-    public MinesweeperView(@NotNull GameController gameController, int width, int height) {
-        this.gameController = gameController;
+    public MinesweeperView(@NotNull GameController controller, int width, int height) {
+        this.controller = controller;
 
         frame = new JFrame("Minesweeper");
         frame.setPreferredSize(new Dimension(width, height));
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.pack();
 
-        var rows = gameController.fieldRows();
-        var cols = gameController.fieldColumns();
+        var rows = controller.fieldRows();
+        var cols = controller.fieldColumns();
         minefieldPanel = new MinefieldPanel(rows, cols);
         minefieldPanel.setLayout(new GridLayout(rows, cols));
         frame.add(minefieldPanel);
 
         frame.setVisible(true);
-        frame.addKeyListener(this);
-        frame.addMouseListener(this);
-        frame.addMouseMotionListener(this);
-        frame.addMouseWheelListener(this);
-    }
-
-    @Override
-    public void keyTyped(KeyEvent e) {
-
-    }
-
-    @Override
-    public void keyPressed(KeyEvent e) {
-        gameController.keyPressed(e.getKeyCode(), e.getKeyLocation(), e.getModifiersEx());
-    }
-
-    @Override
-    public void keyReleased(KeyEvent e) {
-
-    }
-
-    @Override
-    public void mouseClicked(MouseEvent e) {
-
-    }
-
-    @Override
-    public void mousePressed(MouseEvent e) {
-
-    }
-
-    @Override
-    public void mouseReleased(MouseEvent e) {
-
-    }
-
-    @Override
-    public void mouseEntered(MouseEvent e) {
-
-    }
-
-    @Override
-    public void mouseExited(MouseEvent e) {
-
-    }
-
-    @Override
-    public void mouseDragged(MouseEvent e) {
-
-    }
-
-    @Override
-    public void mouseMoved(MouseEvent e) {
-
-    }
-
-    @Override
-    public void mouseWheelMoved(MouseWheelEvent e) {
-
     }
 
 }
